be2ebe186e3e9c38dead26e1f24d73709296ae6c
Ruan <3555748553@qq.com>
2026-07-02 18:54:38 +0800

    checkpoint

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,85 @@
+package deque;
+
+public class ArrayDeque<Item> {
+    private int size;
+    private int head;
+    private int tail;
+    private Item[] items;
+    private int FACTOR = 2;
+
+    ArrayDeque(){
+        size = 0;
+        head = items.length;
+        tail = 0;
+        items = (Item[]) new Object[8];
+    }
+
+    public void addFirst(Item i){
+        if (tail + 1 == head){
+            resize(items.length * FACTOR);
+        }
+        else{
+            items[head] = i;
+            head -= 1;
+            size += 1;
+        }
+    }
+    public void addLast(Item i){
+        if (tail + 1 == head){
+            resize(items.length * FACTOR);
+        }
+        else{
+            items[tail] = i;
+            tail += 1;
+            size += 1;
+        }
+
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    public int size(){
+        return size;
+    }
+    public void printDeque(){
+        for (int i = head; i < items.length; i++){
+            System.out.print(items[i] + " ");
+        }
+        for (int i = 0; i <= tail; i++){
+            System.out.print(items[i] + " ");
+        }
+    }
+    public Item removeFirst(){
+        if (size < items.length / 4){
+            resize(items.length / FACTOR);
+        }
+        Item temp = items[head];
+        head = (head + 1) % items.length;
+        return temp;
+    }
+    public Item removeLast(){
+        if (size < items.length / 4){
+            resize(items.length / FACTOR);
+        }
+        Item temp = items[tail];
+        tail = (tail - 1 + items.length) % items.length;
+        return temp;
+    }
+    public Item get(int index){
+        if (index >= size || index < 0){
+            return null;
+        }
+        else if (index < items.length - head){
+            return items[head + index];
+        }
+        else{
+            return items[index + head - items.length];
+        }
+    }
+    private void resize(int capacity){
+        Item[] newItems = (Item[]) new Object[capacity];
+        System.arraycopy(items, head, newItems, 0, items.length - head);
+        System.arraycopy(items, 0, newItems, items.length - head -1, tail + 1);
+        items = newItems;
+    }
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,181 @@
+package deque;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.List;
+
+import afu.org.checkerframework.checker.igj.qual.I;
+
+public class LinkedListDeque<Item> implements Iterable<Item>{
+    private class ListNode {
+        private Item value;
+        private ListNode next;
+        private ListNode prev;
+
+        public ListNode(Item i, ListNode m, ListNode n){
+            value = i;
+            next = m;
+            prev = n;
+        }
+    }
+
+    private ListNode sentinel;
+    private int size;
+
+    LinkedListDeque(){
+        sentinel = new ListNode(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+    public void addFirst(Item i){
+        if (this.isEmpty()){
+            ListNode first = new ListNode(i, sentinel, sentinel);
+            sentinel.next = first;
+            sentinel.prev = first;
+        }
+        else{
+            ListNode first = new ListNode(i, sentinel.next, sentinel);
+            sentinel.next.prev = first;
+            sentinel.next = first;
+        }
+        size += 1;
+    }
+    public void addLast(Item i){
+        if (this.isEmpty()){
+            ListNode first = new ListNode(i, sentinel, sentinel);
+            sentinel.next = first;
+            sentinel.prev = first;
+        }
+        else{
+            ListNode last = new ListNode(i, sentinel, sentinel.prev);
+            sentinel.prev.next = last;
+            sentinel.prev = last;
+        }
+        size += 1;
+    }
+    public boolean isEmpty() {
+        return (sentinel.next == sentinel);
+    }
+
+    public void printDeque(){
+        ListNode cursor  = sentinel.next;
+        while (cursor != sentinel){
+            cursor = cursor.next;
+            System.out.print(cursor.value + " ");
+        }
+        System.out.println();
+    }
+    public Item removeFirst(){
+        if (size > 1){
+            Item temp = sentinel.next.value;
+            sentinel.next = sentinel.next.next;
+            sentinel.next.prev = sentinel;
+            size -= 1;
+            return temp;
+        }
+        else if (size == 1) {
+            Item temp = sentinel.next.value;
+            sentinel.next = sentinel;
+            sentinel.prev = sentinel;
+            size -= 1;
+            return temp;
+        }
+        else {
+            return null;
+        }
+    }
+    public Item removeLast(){
+        if (size > 1) {
+            Item temp = sentinel.prev.value;
+            sentinel.prev = sentinel.prev.prev;
+            sentinel.prev.next = sentinel;
+            size -= 1;
+            return temp;
+        }
+        else if (size == 1){
+            Item temp = sentinel.prev.value;
+            sentinel.next = sentinel;
+            sentinel.prev = sentinel;
+            size -= 1;
+            return temp;
+        }
+        else{
+            return null;
+        }
+    }
+    public Item get(int index){
+        ListNode cursor = sentinel;
+        for (int i = index; i >= 0; i -= 1){
+            cursor = cursor.next;
+        }
+        Item value = cursor.value;
+        return value;
+    }
+    public int size(){
+        return size;
+    }
+
+    @Override
+    public Iterator<Item> iterator(){
+        return new LLDIterator();
+    }
+
+    private class LLDIterator implements Iterator<Item>{
+        private ListNode cursor = sentinel.next;
+
+        @Override
+        public boolean hasNext() {
+            return (cursor != sentinel);
+        }
+
+        @Override
+        public Item next() {
+            Item value = cursor.value;
+            cursor = cursor.next;
+            return value;
+        }
+    }
+
+    public boolean equals(Object o){
+        if (this == o) {
+            return true;
+        }
+        else if (!(o instanceof LinkedListDeque)){
+            return false;
+        }
+        else{
+            LinkedListDeque<Item> other = (LinkedListDeque<Item>) o;
+            if (this.size == other.size){
+                return false;
+            }
+            else{
+                ListNode otherCursor = other.sentinel.next;
+                for (Item curr : this) {
+                    if (curr != otherCursor.value){
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+
+            }
+        }
+    public Item getRecursive(int index){
+        if (index >= size || index < 0){
+            return null;
+        }
+        else{
+            return getRecursiveHelper(sentinel.next, index);
+        }
+
+    }
+    private Item getRecursiveHelper(ListNode curr, int index){
+        if (index == 0){
+            return curr.value;
+        }
+        else{
+            return getRecursiveHelper(curr.next, index -= 1);
+        }
+    }
+}
